8e919c8bc5c1b52fc6fdeecd03a841371be9089a
JoeyDeckers <deckersjoey@gmail.com>
2020-05-04 23:25:47 +0200

    Debug

--- test/index.test.js
+++ test/index.test.js
@@ -41,10 +41,13 @@ test('Route / exits and render HTML', async (t) => {
 // })
 
 test('Route / exits and render HTML for recipes', async (t) => {
-  const context = {}
-  const { html } = await nuxt.server.renderRoute('/recipes', context)
-  console.log({ html });
-  t.true(html.includes('<h3>Foto test</h3>'))
+  setTimeout(() => {
+    const context = {}
+    const { html } = await nuxt.server.renderRoute('/recipes', context)
+    console.log({ html });
+    t.true(html.includes('<h3>Foto test</h3>'))
+  }, 4000);
+
 })
 // Close server and ask nuxt to stop listening to file changes
 test.after('Closing server and nuxt.js', (t) => {

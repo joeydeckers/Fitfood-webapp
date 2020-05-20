c79cf5afbdaa95ed33c720220832f5501e8b4882
JoeyDeckers <deckersjoey@gmail.com>
2020-05-20 11:37:21 +0200

    Something

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -10,6 +10,7 @@
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-item href="#"><n-link to="/recipes" no-prefetch>Recipes</n-link></b-nav-item>
+          <b-nav-item href="#"><n-link to="/recipes" no-prefetch>Hallo</n-link></b-nav-item>
           <b-nav-item href="#" v-if="token"><n-link to="/recipe/create" no-prefetch>Create recipe</n-link></b-nav-item>
           <b-nav-item href="#" v-if="token"><nuxt-link :to="{name: 'account-id', params:{id: user.id}}" no-prefetch>My account</nuxt-link></b-nav-item>
           <b-nav-item href="#" v-if="!token"><n-link to="/register" no-prefetch>Register</n-link></b-nav-item>

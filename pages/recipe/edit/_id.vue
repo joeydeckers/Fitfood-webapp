d4f5e77882bb46003b1ea6076a83161b39b47bdc
JoeyDeckers <deckersjoey@gmail.com>
2020-06-01 16:56:34 +0200

    Added edit

--- pages/recipe/edit/_id.vue
+++ pages/recipe/edit/_id.vue
@@ -2,7 +2,7 @@
   <div>
       <b-row>
           <b-col lg="5" class="mx-auto">
-            <b-form @submit.prevent="createRecipeByInput(recipeForm)">
+            <b-form @submit.prevent="updateRecipe(recipeForm)">
                 <b-form-group
                     v-for="recipeItem in recipeForm" :key = "recipeItem.id"
                     :label="recipeItem.label"
@@ -49,7 +49,7 @@
                 </b-form-group>
                 <p>Select photo</p>
                 <input class="form-control" style="margin-bottom:20px" data-type="photo" ref="photo" type="file">
-                <b-button type="submit" style="margin-bottom: 30px;">Create recipe</b-button>
+                <b-button type="submit" style="margin-bottom: 30px;">Edit recipe</b-button>
             </b-form>
           </b-col>
       </b-row>
@@ -168,38 +168,103 @@ export default {
         this.$store.dispatch('recipes/getRecipe', this.$route.params.id);
         setTimeout(() => {
             let recipe = this.getRecipe.recipe;
-            console.log(recipe);
+            
+            Object.keys(this.recipeForm).forEach((key, index)=>{
+
+                if( this.recipeForm[key].datatype == "name"){
+                    this.recipeForm[key].model = recipe.name;
+                }
+                if( this.recipeForm[key].datatype == "daytime"){
+                    this.recipeForm[key].model = recipe.category_time;
+                }
+
+                if( this.recipeForm[key].datatype == "description"){
+                    this.recipeForm[key].model = recipe.description;
+                }
+
+                if( this.recipeForm[key].datatype == "wheat-allergy"){
+                    this.recipeForm[key].model = recipe.wheat_allergy;
+                }
+
+                if( this.recipeForm[key].datatype == "milk-allergy"){
+                    this.recipeForm[key].model = recipe.milk_allergy;
+                }
+
+                if( this.recipeForm[key].datatype == "allergydescripion"){
+                    this.recipeForm[key].model = recipe.allergies_list;
+                }
+
+                if( this.recipeForm[key].datatype == "protein"){
+                    this.recipeForm[key].model = recipe.protein;
+                }
+
+                if( this.recipeForm[key].datatype == "carbs"){
+                    this.recipeForm[key].model = recipe.carbs;
+                }
+
+                if( this.recipeForm[key].datatype == "fats"){
+                    this.recipeForm[key].model = recipe.fats;
+                }
+
+                if( this.recipeForm[key].datatype == "calories"){
+                    this.recipeForm[key].model = recipe.calories;
+                }
+                
+            })
         }, 500);
 
     },
     methods:{
-        createRecipeByInput(recipe){
-            this.photo = this.$refs.photo.files[0];
-            let photo = '';
-            var reader = new FileReader();
-            reader.readAsDataURL(this.photo);
-            reader.onload = function () {
-                photo = reader.result;
-            };
-            setTimeout(() => {
-                let recipeToBuild ={
-                    name: recipe[0].model,
-                    description: recipe[1].model, 
-                    wheat_allergy: recipe[2].model, 
-                    milk_allergy: recipe[3].model, 
-                    allergies_list: recipe[4].model, 
-                    owner_id: this.$store.getters.getCurrentUser.id,
-                    votes_id: 1,
-                    comments_id: 1,
-                    category_time: recipe[5].model, 
-                    recipe_photo: photo,
-                    protein: recipe[6].model, 
-                    carbs: recipe[7].model, 
-                    fats: recipe[8].model, 
-                    calories: recipe[9].model, 
-                } 
-            this.$store.dispatch('recipes/createRecipe', {vm:this, recipe:recipeToBuild})
-            }, 500);
+        updateRecipe(recipe){
+            if(this.$refs.photo.files.length !== 0){
+                this.photo = this.$refs.photo.files[0];
+                let photo = '';
+                var reader = new FileReader();
+                reader.readAsDataURL(this.photo);
+                reader.onload = function () {
+                    photo = reader.result;
+                };
+                setTimeout(() => {
+                    let recipeToBuild ={
+                        id: this.$route.params.id,
+                        name: recipe[0].model,
+                        description: recipe[1].model, 
+                        wheat_allergy: recipe[2].model, 
+                        milk_allergy: recipe[3].model, 
+                        allergies_list: recipe[4].model, 
+                        owner_id: this.user.id,
+                        votes_id: 1,
+                        comments_id: 1,
+                        category_time: recipe[5].model, 
+                        recipe_photo: photo,
+                        protein: recipe[6].model, 
+                        carbs: recipe[7].model, 
+                        fats: recipe[8].model, 
+                        calories: recipe[9].model, 
+                    } 
+                 this.$store.dispatch('recipes/updateRecipe', {vm:this, recipe:recipeToBuild})
+                }, 500);
+            }else{
+                setTimeout(() => {
+                    let recipeToBuild ={
+                        id: this.$route.params.id,
+                        name: recipe[0].model,
+                        description: recipe[1].model, 
+                        wheat_allergy: recipe[2].model, 
+                        milk_allergy: recipe[3].model, 
+                        allergies_list: recipe[4].model, 
+                        owner_id: this.user.id,
+                        votes_id: 1,
+                        comments_id: 1,
+                        category_time: recipe[5].model, 
+                        protein: recipe[6].model, 
+                        carbs: recipe[7].model, 
+                        fats: recipe[8].model, 
+                        calories: recipe[9].model, 
+                    } 
+                this.$store.dispatch('recipes/updateRecipe', {vm:this, recipe:recipeToBuild})
+                }, 500);
+            }
         }
     },
     computed:{
--- store/recipes.js
+++ store/recipes.js
@@ -78,6 +78,93 @@ export const actions = {
             commit('SET_RECIPE', response.data)
         })
     },
+
+    async updateRecipe({commit}, {vm, recipe}){
+        if(recipe.wheat_allergy == 'true'){
+            recipe.wheat_allergy = 1;
+        }
+
+        if(recipe.milk_allergy == 'true'){
+            recipe.milk_allergy = 1;
+        }
+
+        if(!recipe.photo_path == null){
+            await axios({
+                method: 'put',
+                url: `${apiRoute}/recipe/${recipe.id}`,
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    Authorization: 'Bearer '+ localStorage.getItem('access_token_fitfood')
+                },
+                data: {
+                    name: recipe.name,
+                    description: recipe.description,
+                    wheat_allergy: recipe.wheat_allergy,
+                    milk_allergy: recipe.milk_allergy,
+                    allergies_list: recipe.allergies_list,
+                    owner_id: recipe.owner_id,
+                    votes_id: recipe.votes_id,
+                    comments_id: recipe.comments_id,
+                    category_time: recipe.category_time,
+                    protein: recipe.protein,
+                    carbs: recipe.carbs,
+                    fats: recipe.fats,
+                    calories: recipe.calories,
+                }
+            })
+            .then((response) =>{
+                vm.$bvToast.toast('Recipe updated', {
+                    title: 'Recipe updated!',
+                    autoHideDelay: 5000,
+                    variant: 'success'
+                })
+                setTimeout(() => {
+                   // this.$router.push({name:'recipes'})
+                }, 2000);
+                //commit('SET_RECIPE', response.data)
+            })
+        }else{
+            await axios({
+                method: 'put',
+                url: `${apiRoute}/recipe/${recipe.id}`,
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                    Authorization: 'Bearer '+ localStorage.getItem('access_token_fitfood')
+                },
+                data: {
+                    name: recipe.name,
+                    description: recipe.description,
+                    wheat_allergy: recipe.wheat_allergy,
+                    milk_allergy: recipe.milk_allergy,
+                    allergies_list: recipe.allergies_list,
+                    owner_id: recipe.owner_id,
+                    votes_id: recipe.votes_id,
+                    comments_id: recipe.comments_id,
+                    category_time: recipe.category_time,
+                    photo_path: recipe.recipe_photo,
+                    protein: recipe.protein,
+                    carbs: recipe.carbs,
+                    fats: recipe.fats,
+                    calories: recipe.calories,
+                }
+            })
+            .then((response) =>{
+                vm.$bvToast.toast('Recipe updated', {
+                    title: 'Recipe updated!',
+                    autoHideDelay: 5000,
+                    variant: 'success'
+                })
+                setTimeout(() => {
+                   // this.$router.push({name:'recipes'})
+                }, 2000);
+                //commit('SET_RECIPE', response.data)
+            })
+        }
+
+
+    },
     async filterRecipe({commit}, filter){
         await axios.post(`${apiRoute}/recipe/filter`,{
             carbs: filter.carbs,
